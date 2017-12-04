1e67ddedf3f914bb692c96b9b214b2196e93ca22
Benjamin Berman <bdberm@gmail.com>
2017-12-04 15:11:45 -0500

    add min max stack

--- stacks_and_queues/min_max_stack.js
+++ stacks_and_queues/min_max_stack.js
@@ -14,14 +14,14 @@ class MinMaxStack {
     if (stackLength === 0) {
       [elWithMinMax["max"], elWithMinMax["min"]] = [el, el];
     } else {
-      const stackTop = this.stack[stackLength - 1];
+      const stackTop = this.stack.store[stackLength - 1];
       if(el > stackTop["max"]) {
         [elWithMinMax["max"], elWithMinMax["min"]] = [el, stackTop["min"]];
 
       } else if (el < stackTop["min"]) {
         [elWithMinMax["max"], elWithMinMax["min"]] = [stackTop["max"], el];
       } else {
-        [elWithMinMax["max"], elWithMinMax["min"]] = [stackTop["max"], stackTop["max"]];
+        [elWithMinMax["max"], elWithMinMax["min"]] = [stackTop["max"], stackTop["min"]];
       }
     }
     this.stack.push(elWithMinMax);
@@ -32,13 +32,49 @@ class MinMaxStack {
   }
 
   max() {
-    return this.stack[this.stack.length() - 1]["max"];
+    if (this.stack.length() === 0) {
+      return null;
+    }
+    return this.stack.store[this.stack.length() - 1]["max"];
   }
 
   min() {
-    return this.stack[this.stack.length() - 1]["min"];
+    if (this.stack.length() === 0) {
+      return null;
+    }
+    return this.stack.store[this.stack.length() - 1]["min"];
   }
 
 }
 
-const myMinMax = new MinMaxStack();
+const myMinMax = new MinMaxStack;
+
+myMinMax.push(2);
+myMinMax.push(2);
+myMinMax.push(4);
+myMinMax.push(6);
+myMinMax.push(4);
+myMinMax.push(6);
+myMinMax.push(1);
+myMinMax.push(2);
+myMinMax.push(1);
+
+//max is 6 min is 1
+console.log(myMinMax.max() === 6);
+console.log(myMinMax.min() === 1);
+
+myMinMax.pop();
+//max is 6 min is 1
+console.log(myMinMax.max() === 6);
+console.log(myMinMax.min() === 1);
+
+myMinMax.pop();
+myMinMax.pop();
+console.log(myMinMax.max() === 6);
+console.log(myMinMax.min() === 2);
+
+myMinMax.pop();
+myMinMax.pop();
+myMinMax.pop();
+console.log(myMinMax.max() === 4);
+console.log(myMinMax.min() === 2);
