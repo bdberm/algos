abdf65658c98b954766a455528443e1d87612929
Benjamin Berman <bdberm@gmail.com>
2017-12-31 19:05:29 -0500

    comment out tests

--- trees/first_common_ancestor.rb
+++ trees/first_common_ancestor.rb
@@ -48,37 +48,39 @@ def find_descendant(parent, descendantA, descendantB=nil)
   nil
 end
 
-root = BinaryNode.new("A")
-root.left = BinaryNode.new("B")
-root.left.parent = root
-root.right = BinaryNode.new("C")
-root.right.parent = root
-root.left.left = BinaryNode.new("D")
-root.left.left.parent = root.left
-root.left.right = BinaryNode.new("E")
-root.left.right.parent = root.left
-root.right.left = BinaryNode.new("F")
-root.right.left.parent = root.right
-root.right.right = BinaryNode.new("G")
-root.right.right.parent = root.right
-root.left.left.left = BinaryNode.new("H")
-root.left.left.left.parent = root.left.left
-root.left.left.right = BinaryNode.new("I")
-root.left.left.right.parent = root.left.left
-root.left.right.left = BinaryNode.new("J")
-root.left.right.left.parent = root.left.right
-root.left.right.right = BinaryNode.new("K")
-root.left.right.right.parent = root.left.right
-root.right.left.left = BinaryNode.new("L")
-root.right.left.left.parent = root.right.left
-root.right.left.right = BinaryNode.new("M")
-root.right.left.right.parent = root.right.left
-root.right.right.left = BinaryNode.new("N")
-root.right.right.left.parent = root.right.right
-root.right.right.right = BinaryNode.new("O")
-root.right.right.left.parent = root.right.right
+#Test
 
-puts(first_common_ancestor(root, root.left.right.left, root.left.right.right).val === "E")
-puts(first_common_ancestor(root, root.left.right.left, root.left.left).val === "B")
-puts(first_common_ancestor(root, root.left.right.left, root.right.left.left).val === "A")
-puts(first_common_ancestor(root, root.left.right.left, root.left.left.left).val === "B")
+# root = BinaryNode.new("A")
+# root.left = BinaryNode.new("B")
+# root.left.parent = root
+# root.right = BinaryNode.new("C")
+# root.right.parent = root
+# root.left.left = BinaryNode.new("D")
+# root.left.left.parent = root.left
+# root.left.right = BinaryNode.new("E")
+# root.left.right.parent = root.left
+# root.right.left = BinaryNode.new("F")
+# root.right.left.parent = root.right
+# root.right.right = BinaryNode.new("G")
+# root.right.right.parent = root.right
+# root.left.left.left = BinaryNode.new("H")
+# root.left.left.left.parent = root.left.left
+# root.left.left.right = BinaryNode.new("I")
+# root.left.left.right.parent = root.left.left
+# root.left.right.left = BinaryNode.new("J")
+# root.left.right.left.parent = root.left.right
+# root.left.right.right = BinaryNode.new("K")
+# root.left.right.right.parent = root.left.right
+# root.right.left.left = BinaryNode.new("L")
+# root.right.left.left.parent = root.right.left
+# root.right.left.right = BinaryNode.new("M")
+# root.right.left.right.parent = root.right.left
+# root.right.right.left = BinaryNode.new("N")
+# root.right.right.left.parent = root.right.right
+# root.right.right.right = BinaryNode.new("O")
+# root.right.right.left.parent = root.right.right
+#
+# puts(first_common_ancestor(root, root.left.right.left, root.left.right.right).val === "E")
+# puts(first_common_ancestor(root, root.left.right.left, root.left.left).val === "B")
+# puts(first_common_ancestor(root, root.left.right.left, root.right.left.left).val === "A")
+# puts(first_common_ancestor(root, root.left.right.left, root.left.left.left).val === "B")
